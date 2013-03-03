e059cfca48bf8bcdde48986c1d9bc272ec3dfd2f
Albert_12 <albert.postal@gmail.com>
2013-03-04 02:48:49 +0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+
+
+group :development do
+  gem 'annotate', '2.5.0'
+  gem 'sqlite3', '1.3.5'
+end
+
+gem 'jquery-rails', '2.0.2'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenBoard::Application.config.secret_token = 'f3e7298e0ebb7cc61c370a800d33b88b0948fe0076be96dd1df04bfe6cdaec7d78ad8f2da9f090190bfa792b4e6ba81d8a61bb20a1d262d3c6625efb94c7d8a8'
