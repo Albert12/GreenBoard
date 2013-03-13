80ef4658776e2b8a721669947114a5346c4bf8b3
Albert_12 <albert.postal@gmail.com>
2013-03-14 01:22:04 +0400

    ready for install ActiveAdmin

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -7,7 +7,7 @@
 
 class Advert < ActiveRecord::Base
   attr_accessible :content, :email, :phone, :icq, :skype, :name, :category_id
-  validates :content, presence: true
+  validates :content, :category_id, presence: true
   VALID_PHONE_REGEX = /\d/
   validates :phone, presence: true, length: { minimum: 7, maximum: 11 },
   					format: { with: VALID_PHONE_REGEX }
